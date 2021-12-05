6c60f1777ef83d0e4d6acc1a458e9ad5a1e9ef7b
powHexi <47748365+powHexi@users.noreply.github.com>
2021-12-05 18:14:12 +0200

    Edited config

--- playwright.config.js
+++ playwright.config.js
@@ -2,11 +2,8 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
     use: {
-      headless: false,
-      viewport: { width: 1280, height: 720 },
-      channel: "chrome",
-      video: 'retain-on-failure',
-      trace: 'retain-on-failure'
+      headless: true,
+      channel: "chrome"
     },
   };
   
